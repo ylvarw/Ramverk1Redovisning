8cf5aa3ac4d9e516ab7fa3ab5944f59b305a8fa7
ylvarw <ylvarw@gmail.com>
2020-11-30 16:56:11 +0100

    moved helpers to controller folder, renamed class

---
+++ src/Controller/IpPosition.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Ylvan\Controller;
+
+// include './vars.php';
+// require 'vars.php';
+
+
+/**
+ * A class to handle requests for geolocation
+ */
+class IpPosition
+{
+
+    /**
+     * recieve the IP and return coordinates
+     */
+    public function getPosition($ip)
+    {
+        // connect to api and get response
+        $result = $this->connectApi($ip);
+        return $result;
+    }
+
+
+
+    /**
+     * make API call, return result for IP
+     */
+    private function connectApi($ip)
+    {
+        $access_key = '5d3399b227dff07cac9f896eaa07ea71';
+        $url = 'http://api.ipstack.com/';
+        $response = file_get_contents($url . $ip . '?access_key=' . $access_key . '&format=1');
+        $api_result = json_decode($response, true);
+
+        return $api_result;
+    }
+}
