9e1e2674fb16fb3c973e634b0ec92c99048d22be
ylvarw <ylvarw@gmail.com>
2020-12-01 15:36:22 +0100

    tests for geolocations

--- test/Controller/IpPositionTest.php
+++ test/Controller/IpPositionTest.php
@@ -4,7 +4,6 @@ namespace Ylvan\Controller;
 
 use Anax\DI\DIFactoryConfig;
 use PHPUnit\Framework\TestCase;
-// use Ylvan\Ip\IpHandler;
 
 /**
  * Test the helprt class IpHandler.
@@ -12,9 +11,8 @@ use PHPUnit\Framework\TestCase;
 class IpPositionTest extends TestCase
 {
 
-
     /**
-     * Test get user IP
+     * Test get position of IP
      */
     public function testGetposition()
     {
@@ -26,67 +24,4 @@ class IpPositionTest extends TestCase
         $res = $geo->getPosition($ip);
         $this->assertNotNull($res);
     }
-
-
-    // /**
-    //  * Test get user IP
-    //  */
-    // public function testGetpositionTypr()
-    // {
-    //     // get testData
-    //     $ipClass = new IpHandler();
-    //     $ip = $ipClass->getUserIp();
-        
-    //     $geo = new GeoTag();
-    //     $res = $geo->getPosition($ip);
-    //     $this->assertIsArray($res);
-    // }
-
-
-    // /**
-    //  * Test get user IP
-    //  */
-    // public function testGetUserIp()
-    // {
-    //     $ipClass = new IpHandler();
-    //     // $ipClass->ipv4();
-    //     $res = $ipClass->getUserIp();
-    //     $this->assertIsString($res);
-    // }
-
-
-    // /**
-    //  * Test the ipv4 validation method.
-    //  */
-    // public function testIpv4Invalid()
-    // {
-    //     $ipClass = new IpHandler();
-    //     // $ipClass->ipv4();
-    //     $res = $ipClass->ipv4("invalid");
-    //     $this->assertContains("Validerar ej", $res);
-    // }
-
-
-    // /**
-    //  * Test the ipv4 validation method.
-    //  */
-    // public function testIpv4Valid()
-    // {
-    //     $ipClass = new IpHandler();
-    //     // $ipClass->ipv4();
-    //     $res = $ipClass->ipv4("8.8.8.8");
-    //     $this->assertContains("Validerar", $res);
-    // }
-
-
-    // /**
-    //  * Test the ipv4 validation method.
-    //  */
-    // public function testIpv4Type()
-    // {
-    //     $ipClass = new IpHandler();
-    //     // $ipClass->ipv4();
-    //     $res = $ipClass->ipv4("8.8.8.8");
-    //     $this->assertIsString($res);
-    // }
 }
