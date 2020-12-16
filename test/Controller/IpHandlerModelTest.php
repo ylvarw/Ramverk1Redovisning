d9fc28ebd6ef69e1ef6e89ea4807b4a5093423c6
ylvarw <ylvarw@gmail.com>
2020-12-16 16:51:26 +0100

    tests model class for ip handling

---
+++ test/Controller/IpHandlerModelTest.php
@@ -0,0 +1,184 @@
+<?php
+
+namespace Ylvan\Models;
+
+// require 'helpers/IpHandler.php';
+
+use Anax\DI\DIFactoryConfig;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test the helprt class IpHandler.
+ */
+class IpHandlerTest extends TestCase
+{
+
+    /**
+     * Test get user IP
+     */
+    public function testGetUserIpContent()
+    {
+        $ipClass = new IpHandler();
+        // $ipClass->ipv4();
+        $res = $ipClass->getUserIp();
+        $this->assertNotNull($res);
+    }
+
+
+    /**
+     * Test get user IP
+     */
+    public function testGetUserIp()
+    {
+        $ipClass = new IpHandler();
+        // $ipClass->ipv4();
+        $res = $ipClass->getUserIp();
+        $this->assertIsString($res);
+    }
+
+
+    /**
+     * Test the general validation method using ipv4.
+     */
+    public function testIpIsvalidIpv4()
+    {
+        $ipClass = new IpHandler();
+        // $ipClass->ipv4();
+        $res = $ipClass->ipIsValid("8.8.8.8");
+        $this->assertIsTrue($res);
+    }
+
+
+    /**
+     * Test the general validation method using ipv6.
+     */
+    public function testIpIsvalidIpv6()
+    {
+        $ipClass = new IpHandler();
+        // $ipClass->ipv4();
+        $res = $ipClass->ipIsValid("2001:4860:4860::8888");
+        $this->assertIsTrue($res);
+    }
+
+
+    /**
+     * Test the general validation method using ipv6.
+     */
+    public function testIpIsvalidFalse()
+    {
+        $ipClass = new IpHandler();
+        // $ipClass->ipv4();
+        $res = $ipClass->ipIsValid("123");
+        $this->assertIsFalse($res);
+    }
+
+
+    /**
+     * Test the ipv4 validation method.
+     */
+    public function testIpv4Invalid()
+    {
+        $ipClass = new IpHandler();
+        // $ipClass->ipv4();
+        $res = $ipClass->ipv4("invalid");
+        $this->assertContains("Validerar ej", $res);
+    }
+
+
+    /**
+     * Test the ipv4 validation method.
+     */
+    public function testIpv4Valid()
+    {
+        $ipClass = new IpHandler();
+        // $ipClass->ipv4();
+        $res = $ipClass->ipv4("8.8.8.8");
+        $this->assertContains("Validerar", $res);
+    }
+
+
+    /**
+     * Test the ipv4 validation method.
+     */
+    public function testIpv4Type()
+    {
+        $ipClass = new IpHandler();
+        // $ipClass->ipv4();
+        $res = $ipClass->ipv4("8.8.8.8");
+        $this->assertIsString($res);
+    }
+
+
+    /**
+     * Test the ipv6 validation method.
+     */
+    public function testIpv6Invalid()
+    {
+        $ipClass = new IpHandler();
+        // $ipClass->ipv6();
+        $res = $ipClass->ipv6("invalid");
+        $this->assertContains("Validerar ej", $res);
+    }
+
+
+
+    /**
+     * Test the ipv6 validation method.
+     */
+    public function testIpv6Valid()
+    {
+        $ipClass = new IpHandler();
+        // $ipClass->ipv6();
+        $res = $ipClass->ipv6("2001:4860:4860::8888");
+        $this->assertContains("Validerar", $res);
+    }
+
+
+    /**
+     * Test the ipv6 validation method.
+     */
+    public function testIpv6Type()
+    {
+        $ipClass = new IpHandler();
+        // $ipClass->ipv6();
+        $res = $ipClass->ipv6("2001:4860:4860::8888");
+        $this->assertIsString($res);
+    }
+
+
+    /**
+     * Test the domain name checker method.
+     */
+    public function testDomainNameInvalid()
+    {
+        $ipClass = new IpHandler();
+        // $ipClass->domainName();
+        $res = $ipClass->domainName("invalid");
+        $this->assertContains("ip ej validerad", $res);
+    }
+
+
+    /**
+     * Test the domain name checker method.
+     */
+    public function testDomainNameValid()
+    {
+        $ipClass = new IpHandler();
+        // $ipClass->domainName();
+        $res = $ipClass->domainName("8.8.8.8");
+        $this->assertContains("dns.google", $res);
+    }
+
+
+    /**
+     * Test the domain name checker method.
+     */
+    public function testDomainNameType()
+    {
+        $ipClass = new IpHandler();
+        // $ipClass->domainName();
+        $res = $ipClass->domainName("8.8.8.8");
+        $this->assertIsString($res);
+    }
+
+}
