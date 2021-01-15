50ff42c970c247282b39f5c66f2964d13880b869
ylvarw <ylvarw@gmail.com>
2021-01-15 11:46:17 +0100

    new report added

--- config/navbar/header.php
+++ config/navbar/header.php
@@ -40,6 +40,11 @@ return [
                         "url" => "redovisning/kmom04",
                         "title" => "Redovisning för kmom04.",
                     ],
+                    [
+                        "text" => "Kmom05",
+                        "url" => "redovisning/kmom05",
+                        "title" => "Redovisning för kmom05.",
+                    ],
                 ],
             ],
         ],
